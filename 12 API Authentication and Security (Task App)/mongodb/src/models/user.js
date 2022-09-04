e9c9f59eb0566622a6353b2673b2fd6e1ef38c80
mamad <mohammadtt77@yahoo.com>
2022-09-04 20:39:05 +0430

    logging in User

--- 12 API Authentication and Security (Task App)/mongodb/src/models/user.js
+++ 12 API Authentication and Security (Task App)/mongodb/src/models/user.js
@@ -1,7 +1,7 @@
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
-
-const User = mongoose.model("user", {
+const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
@@ -15,6 +15,7 @@ const User = mongoose.model("user", {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -39,5 +40,31 @@ const User = mongoose.model("user", {
     },
   },
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
+// use pre for using this before saving
+// hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  console.log(this);
+  console.log("just before saving!");
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
--- 12 API Authentication and Security (Task App)/mongodb/src/routes/user-router.js
+++ 12 API Authentication and Security (Task App)/mongodb/src/routes/user-router.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
@@ -71,10 +83,13 @@ router.patch("/users/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid Updates" });
   }
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
     if (!user) {
       return res.status(404).send;
     }
