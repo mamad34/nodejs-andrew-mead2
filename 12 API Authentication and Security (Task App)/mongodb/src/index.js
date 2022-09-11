c0f37af7aadf70511449e9aa330b3aaf597be088
mamad <mohammadtt77@yahoo.com>
2022-09-11 22:30:47 +0430

    111 log out

--- 12 API Authentication and Security (Task App)/mongodb/src/index.js
+++ 12 API Authentication and Security (Task App)/mongodb/src/index.js
@@ -7,9 +7,29 @@ const taskRouter = require("./routes/task-route");
 
 const app = express();
 const port = process.env.PORT || 3000;
+// express middle ware
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET Request are disabled");
+//   } else {
+//     // if dont call next the route never runs
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("We are updating the site thanks for your patinet");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+//
+// Without middleware : new requset --> run route handler
+//
+// with middleware : new request -> do somthing -> run route handler
+//
 
 // const router = new express.Router();
 // router.get("/test", (req, res) => {
@@ -21,14 +41,24 @@ app.listen(port, () => {
   console.log("Server is Up on port " + port);
 });
 
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+// const myFunction = async () => {
+//   const password = "Red12345!";
+//   const hashedPassword = await bcrypt.hash(password, 8);
+//   console.log(password);
+//   console.log(hashedPassword);
+//   const isMatch = await bcrypt.compare("red12345!", hashedPassword);
+//   console.log(isMatch);
+// };
+// myFunction();
 
+const jwt = require("jsonwebtoken");
 const myFunction = async () => {
-  const password = "Red12345!";
-  const hashedPassword = await bcrypt.hash(password, 8);
-  console.log(password);
-  console.log(hashedPassword);
-  const isMatch = await bcrypt.compare("red12345!", hashedPassword);
-  console.log(isMatch);
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewcource", {
+    expiresIn: "7 days",
+  });
+  console.log(token);
+  const data = jwt.verify(token, "thisismynewcource");
+  console.log(data);
 };
 myFunction();
--- 12 API Authentication and Security (Task App)/mongodb/src/routes/user-router.js
+++ 12 API Authentication and Security (Task App)/mongodb/src/routes/user-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 router.post("/users/login", async (req, res) => {
   try {
@@ -8,7 +9,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    // res.send(user);
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -19,7 +22,8 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -34,13 +38,8 @@ router.post("/users", async (req, res) => {
   //   });
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
   // User.find({})
   //   .then((users) => {
   //     res.send(users);
@@ -71,6 +70,29 @@ router.get("/users/:id", async (req, res) => {
   //   .catch((e) => {
   //     res.status(500).send();
   //   });
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 router.patch("/users/:id", async (req, res) => {
